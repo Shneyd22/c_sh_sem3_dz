60aca3ad34a720f16a65865ee0e1898462311dcd
Sergei Schneidmiller <5797221@gmail.com>
2023-03-19 13:22:57 +0300

    dz3 ready

---
+++ dz3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+// try
+// {
+    Console.WriteLine("Введите число");
+    int N = Convert.ToInt32(Console.ReadLine());
+    NumCubTable (N);      //Почему-то с проверкой функция не работает
+// }
+// catch
+// {
+//     Console.WriteLine("Введено некорректное значение");
+// }
+
+
+
+
+
+void NumCubTable (int i)
+{
+    for (i = 1; i < N; i++)
+    {
+        int cub = i * i * i;
+        Console.Write($"{cub}, ");
+    }
+    if (i == N) Console.WriteLine(N * N * N);
+}
